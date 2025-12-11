161bb18cd8b0182c64d5c65e281492b00b94f214
pdimarcodev <pablo.d.dimarco@gmail.com>
2025-12-11 17:30:23 +0100

    feat: detail info

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,34 +1,46 @@
-import { Screen } from '@/src/components/Screen'
-import { Text } from "@/src/components/Text"
-import { CityDetailsHeader } from "@/src/containers/CityDetailsHeader"
-import { CityDetailsInfo } from "@/src/containers/CityDetailsInfo"
-import { CityDetailsMap } from "@/src/containers/CityDetailsMap"
-import { CityDetailsRelatedCities } from "@/src/containers/CityDetailsRelatedCities"
-import { CityDetailsTouristAttractions } from "@/src/containers/CityDetailsTouristAttractions"
-import { useCityDetails } from "@/src/data/useCityDetails"
-import { useLocalSearchParams } from 'expo-router'
+import { Divider } from "@/src/components/Divider";
+import { Screen } from "@/src/components/Screen";
+import { Text } from "@/src/components/Text";
+import { CityDetailsHeader } from "@/src/containers/CityDetailsHeader";
+import { CityDetailsInfo } from "@/src/containers/CityDetailsInfo";
+import { CityDetailsMap } from "@/src/containers/CityDetailsMap";
+import { CityDetailsRelatedCities } from "@/src/containers/CityDetailsRelatedCities";
+import { CityDetailsTouristAttractions } from "@/src/containers/CityDetailsTouristAttractions";
+import { useCityDetails } from "@/src/data/useCityDetails";
+import { useLocalSearchParams } from "expo-router";
 
 export default function CityDetails() {
-  const { id } = useLocalSearchParams<{ id: string }>()
-  const city = useCityDetails(id)
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const city = useCityDetails(id);
 
-  if (!city) return (
-    <Screen flex={1} justifyContent="center" alignItems="center">
-      <Text>City not found</Text>
-    </Screen>
-  )
+  if (!city) {
+    return (
+      <Screen flex={1} justifyContent="center" alignItems="center">
+        <Text>City not found</Text>
+      </Screen>
+    );
+  }
 
   return (
     <Screen style={{ paddingHorizontal: 0 }}>
       <CityDetailsHeader
         id={city.id}
-        categories={city.categories}
         coverImage={city.coverImage}
+        categories={city.categories}
       />
-      <CityDetailsInfo />
-      <CityDetailsMap />
-      <CityDetailsRelatedCities />
+      <CityDetailsInfo
+        name={city.name}
+        country={city.country}
+        description={city.description}
+      />
+      <Divider paddingHorizontal="padding" />
       <CityDetailsTouristAttractions />
+
+      <Divider paddingHorizontal="padding" />
+      <CityDetailsMap />
+
+      <Divider paddingHorizontal="padding" />
+      <CityDetailsRelatedCities />
     </Screen>
-  )
+  );
 }
