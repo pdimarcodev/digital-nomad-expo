b7982011c4cfa00811ae21e82e6ab30c17c709d9
pdimarcodev <pablo.d.dimarco@gmail.com>
2025-12-11 16:02:05 +0100

    feat: city detail header

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,23 +1,34 @@
 import { Screen } from '@/src/components/Screen'
+import { Text } from "@/src/components/Text"
 import { CityDetailsHeader } from "@/src/containers/CityDetailsHeader"
 import { CityDetailsInfo } from "@/src/containers/CityDetailsInfo"
 import { CityDetailsMap } from "@/src/containers/CityDetailsMap"
 import { CityDetailsRelatedCities } from "@/src/containers/CityDetailsRelatedCities"
 import { CityDetailsTouristAttractions } from "@/src/containers/CityDetailsTouristAttractions"
-import { useLocalSearchParams, useRouter } from 'expo-router'
+import { useCityDetails } from "@/src/data/useCityDetails"
+import { useLocalSearchParams } from 'expo-router'
 
 export default function CityDetails() {
-  const router = useRouter()
-  const { id } = useLocalSearchParams()
+  const { id } = useLocalSearchParams<{ id: string }>()
+  const city = useCityDetails(id)
+
+  if (!city) return (
+    <Screen flex={1} justifyContent="center" alignItems="center">
+      <Text>City not found</Text>
+    </Screen>
+  )
 
   return (
-    <Screen>
-      <CityDetailsHeader />
+    <Screen style={{ paddingHorizontal: 0 }}>
+      <CityDetailsHeader
+        id={city.id}
+        categories={city.categories}
+        coverImage={city.coverImage}
+      />
       <CityDetailsInfo />
       <CityDetailsMap />
       <CityDetailsRelatedCities />
       <CityDetailsTouristAttractions />
     </Screen>
-
   )
 }
--- src/containers/CityDetailsHeader.tsx
+++ src/containers/CityDetailsHeader.tsx
@@ -1,14 +1,41 @@
+import { useRouter } from "expo-router";
+import { ImageBackground } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Box } from "../components/Box";
-import { Text } from "../components/Text";
+import { Icon } from "../components/Icon";
+import { IconButton } from "../components/IconButton";
+import { City } from "../types";
 
+type CityDetailsHeaderProps = Pick<City,
+  | "id" | "categories" | "coverImage">
 
-
-export function CityDetailsHeader() {
+export function CityDetailsHeader({ id, categories, coverImage }: CityDetailsHeaderProps) {
+  const router = useRouter()
+  const { top } = useSafeAreaInsets()
 
   return (
     <Box>
-      <Text>CityDetailsHeader</Text>
+      <ImageBackground
+        source={coverImage}
+        style={{
+          width: "100%",
+          height: 250,
+        }}
+        imageStyle={{
+          borderBottomRightRadius: 40
+        }}
+      >
+        <Box
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding="padding"
+          style={{ paddingTop: top }}
+        >
+          <IconButton iconName="Chevron-left" onPress={router.back} />
+          <Icon name="Favorite-outline" size={30} color="pureWhite" />
+        </Box>
+      </ImageBackground>
     </Box>
   )
-
 }
