62a08c14ccfa320df385a392576b343ed94c8584
pdimarcodev <pablo.d.dimarco@gmail.com>
2026-01-07 16:59:28 +0100

    feat: button comp

--- src/ui/components/Box.tsx
+++ src/ui/components/Box.tsx
@@ -1,5 +1,38 @@
-import { createBox } from '@shopify/restyle';
-import { Theme } from '../theme/theme';
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from "@shopify/restyle";
+import {
+  TouchableOpacity as RNTouchableOpacity,
+  TouchableOpacityProps as RNTouchableOpacityProps,
+} from "react-native";
+import { Theme } from "../theme/theme";
 
 export const Box = createBox<Theme>();
-export type BoxProps = React.ComponentProps<typeof Box>
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+type RestyleStyle = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme>;
+
+export type TouchableOpacityBoxProps = RNTouchableOpacityProps & RestyleStyle;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBox,
+  Theme
+>(
+  [backgroundColor, spacing, layout, border, spacingShorthand],
+  RNTouchableOpacity
+);
---
+++ src/ui/components/Button.tsx
@@ -0,0 +1,48 @@
+import { ThemeColors } from "../theme/theme";
+import { TouchableOpacityBox, TouchableOpacityBoxProps } from "./Box";
+import { Text } from "./Text";
+
+type ButtonVariant = "primary" | "secondary";
+
+const buttonColors: Record<
+  ButtonVariant,
+  { backgroundColor: ThemeColors; textColor: ThemeColors }
+> = {
+  primary: {
+    backgroundColor: "primary",
+    textColor: "text",
+  },
+  secondary: {
+    backgroundColor: "gray1",
+    textColor: "text",
+  },
+};
+
+type ButtonProps = TouchableOpacityBoxProps & {
+  title: string;
+  onPress: () => {};
+  variant?: ButtonVariant;
+};
+
+export function Button({
+  title,
+  onPress,
+  variant = "primary",
+  ...touchableOpacityProps
+}: ButtonProps) {
+  const buttonProps = buttonColors[variant];
+
+  return (
+    <TouchableOpacityBox
+      {...touchableOpacityProps}
+      onPress={onPress}
+      backgroundColor={buttonProps.backgroundColor}
+      borderRadius="default"
+      padding="padding"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Text color={buttonProps.textColor}>{title}</Text>
+    </TouchableOpacityBox>
+  );
+}
