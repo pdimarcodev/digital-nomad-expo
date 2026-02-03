9f2f1e6673d9363544daa9a5f37f9e00ad31c76d
pdimarcodev <pablo.d.dimarco@gmail.com>
2026-02-03 08:49:23 +0100

    feat: clear cache on sign out

--- src/domain/auth/operations/useAuthSignOut.ts
+++ src/domain/auth/operations/useAuthSignOut.ts
@@ -1,14 +1,18 @@
 import { useAppMutation } from "@/src/infra/operations/useAppMutation";
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
+import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../AuthContext";
 
 export function useAuthSignOut() {
   const { auth } = useRepository();
   const { removeAuthUser } = useAuth();
 
+  const queryClient = useQueryClient();
+
   return useAppMutation({
     mutateFn: () => auth.signOut(),
     onSuccess: () => {
+      queryClient.clear();
       removeAuthUser();
     },
   });
