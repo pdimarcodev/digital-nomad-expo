ece9ded64ecd4a9925d23fdf5e354a225cf80bf1
pdimarcodev <pablo.d.dimarco@gmail.com>
2026-02-06 10:12:34 +0100

    feat: city details fade in

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -10,59 +10,71 @@ import { CityDetailsRelatedCities } from "@/src/ui/containers/CityDetailsRelated
 import { CityDetailsTouristAttractions } from "@/src/ui/containers/CityDetailsTouristAttractions";
 import { useLocalSearchParams } from "expo-router";
 import { Pressable } from "react-native";
-import { useSharedValue } from "react-native-reanimated";
+import Animated, { FadeIn, useSharedValue } from "react-native-reanimated";
+
+const PAGE_ANIMATION_TIME = 1000;
 
 export default function CityDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const { data: city } = useCityFindById(id);
+  const { data: city, error, isLoading } = useCityFindById(id);
 
   const bottomSheetIsOpen = useSharedValue(false);
-
   function toggleBottomSheet() {
     bottomSheetIsOpen.value = !bottomSheetIsOpen.value;
   }
 
-  if (!city) {
+  if (isLoading) {
     return (
       <Screen flex={1} justifyContent="center" alignItems="center">
-        <Text>City not found</Text>
+        <Text>Carregando dados...</Text>
+      </Screen>
+    );
+  }
+
+  if (error || !city) {
+    return (
+      <Screen flex={1} justifyContent="center" alignItems="center">
+        <Text>Erro ao carregar cidade</Text>
       </Screen>
     );
   }
 
   return (
     <>
-      <Screen scrollable style={{ paddingHorizontal: 0 }}>
-        <CityDetailsHeader
-          id={city.id}
-          coverImage={city.coverImage}
-          categories={city.categories}
-          isFavorite={city.isFavorite}
-        />
-        <CityDetailsInfo
-          name={city.name}
-          country={city.country}
-          description={city.description}
-        />
-        <Divider paddingHorizontal="padding" />
-        <CityDetailsTouristAttractions
-          touristAttractions={city.touristAttractions}
-        />
+      <Screen style={{ paddingHorizontal: 0 }} scrollable>
+        <Animated.View entering={FadeIn.duration(PAGE_ANIMATION_TIME)}>
+          <CityDetailsHeader
+            id={city.id}
+            coverImage={city.coverImage}
+            categories={city.categories}
+            isFavorite={city.isFavorite}
+          />
+          <CityDetailsInfo
+            name={city.name}
+            country={city.country}
+            description={city.description}
+          />
+          <Divider paddingHorizontal="padding" />
+          <CityDetailsTouristAttractions
+            touristAttractions={city.touristAttractions}
+          />
 
-        <Divider paddingHorizontal="padding" />
-        <Pressable onPress={toggleBottomSheet}>
-          <CityDetailsMap location={city.location} />
-        </Pressable>
+          <Divider paddingHorizontal="padding" />
+          <Pressable onPress={toggleBottomSheet}>
+            <CityDetailsMap location={city.location} />
+          </Pressable>
 
-        <Divider paddingHorizontal="padding" />
-        <CityDetailsRelatedCities id={city.id} />
+          <Divider paddingHorizontal="padding" />
+          <CityDetailsRelatedCities id={city.id} />
+        </Animated.View>
       </Screen>
-
-      <BottomSheetMap
-        location={city.location}
-        isOpen={bottomSheetIsOpen}
-        onPress={toggleBottomSheet}
-      />
+      <Animated.View entering={FadeIn.duration(0).delay(PAGE_ANIMATION_TIME)}>
+        <BottomSheetMap
+          location={city.location}
+          isOpen={bottomSheetIsOpen}
+          onPress={toggleBottomSheet}
+        />
+      </Animated.View>
     </>
   );
 }
